71f2a7b925bb979d0d389df533f7ae81dd4c8d1a
Abi-Abdullah Mohammad Nihad <abi-abdullahmohammadnihad@Nihads-MacBook-Air.local>
2026-05-04 12:38:39 +0600

    v22.53: Fix syntax errors, sync admin changes in real-time, update version tags

--- unity-app.js
+++ unity-app.js
@@ -48,7 +48,7 @@ const LOGIN_PASSWORD = "64423";
 const AUTH_KEY = "unity_v16_auth_persistent";
 const AUTH_USER_KEY = "unity_v16_user_persistent";
 const ACCOUNTS_KEY = "unity_accounts";
-const APP_VERSION = "v22.51";
+const APP_VERSION = "v22.53";
 const MASTER_ADMIN = "abi.nihad";
 const UNIVERSAL_PASSWORD = "64423";
 const REMEMBER_KEY = "unity-dashboard-remember-me";
@@ -5763,12 +5763,12 @@ function buildXlsxFiles() {
 </Relationships>`,
     "docProps/app.xml": `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
-  <Application>UNITY Dashboard v22.49</Application>
+  <Application>UNITY Dashboard v22.53</Application>
 </Properties>`,
     "docProps/core.xml": `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <dc:title>${escapeXml(documentFileBaseName())}</dc:title>
-  <dc:creator>UNITY Dashboard v22.49</dc:creator>
+  <dc:creator>UNITY Dashboard v22.53</dc:creator>
   <dcterms:created xsi:type="dcterms:W3CDTF">${now}</dcterms:created>
   <dcterms:modified xsi:type="dcterms:W3CDTF">${now}</dcterms:modified>
 </cp:coreProperties>`,
